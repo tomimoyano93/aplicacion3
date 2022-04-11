370b3034ce9fdef7cbca463a4f4e0edd1362109b
tomimoyano <tomasmoyano93@gmail.com>
2022-04-11 20:15:06 -0300

    Terminando app

--- src/utils/database.js
+++ src/utils/database.js
@@ -1,4 +1,4 @@
-export const URL_API = '';
-const API_KEY = ''
+export const URL_API = 'https://breadappshop-default-rtdb.firebaseio.com/';
+const API_KEY = 'AIzaSyDMm4TPpK_4uoGyUZ2KIkffP-BxWTJ8Dr0';
 export const URL_AUTH_SIGNUP = `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${API_KEY}`;
 export const URL_AUTH_SIGNIN = `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${API_KEY}`;
